8722afc156db1e1bde41ce0d6c2faced83cff210
Akash-Pardasani <smartavi004@gmail.com>
2017-11-28 23:17:20 +0530

    modified temporal memory

--- temporal_memory.py
+++ temporal_memory.py
@@ -13,24 +13,28 @@ class temporal_memory(self, input_space, num_iter, columns, syn_perm_up, syn_per
 	self.syn_perm_up = syn_perm_up
 	self.num_distals = num_distals
 	self.syn_perm_down = syn_perm_down
-	self.distal_columns = numpy.array(self.num_columns, self.num_distals, 2)
+	self.distal_columns = numpy.array(self.num_columns, self.num_column_cells, 3)
 	self.column_space = numpy.array(self.num_columns, self.num_column_cells)
 	self.column_setup()
 	self.learn_sequence(self.input_sequence)
+	self.predictive_flag = numpy.array(self.num_columns, self.num_column_cells, 1)
+	self.on_configuration = numpy.array(self.num_columns, self.num_columns, 
 
 	def column_setup(self):
 		for i in xrange(self.num_columns):	
 			for k in xrange(self.num_column_cells)
-			distal = random.sample(xrange(self.num_columns), self.num_distals)
-			for j in xrange(self.num_distals):
-				self.distal_columns[i][j][0] = distal[j]
-				self.distal_columns[i][j][1] = self.default_perm	
+				dist_columns = random.sample(xrange(self.num_columns), self.num_distals)  #first select distal columns, then one cell per distal columm. Assuming that a cell can have
+				for j in dist_columns:					    		  #connections only to one cell per column
+					self.distal_columns[i][k][0] = j
+					self.distal_columns[i][k][1] = randint(xrange(self.num_column_cells))
+					self.distal_columns[i][k][2] = self.default_perm	
 
 
 	def learn_sequence(self, input_sequence):
 		for i in xrange(num_iter):
 			for j in input_sequence:
-				if j != input_sequence[0]:
-					for k in j:                   # on sdr columns of the input sdr
-						for m in num_distals: # match if these columns are also distally connected to a column
-							if 
+					for k in j:                   					# on sdr columns of the input sdr
+						for m in self.num_column_cells: 			# match if these columns are also distally connected to a column
+							if self.predictive_flag[k][m][0] == 1:      	#if there is already a predictive cell in the column, just fire that
+								on_configuration.append((k, m))
+						
